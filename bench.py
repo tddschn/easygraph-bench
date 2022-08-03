3cf72c03a151fae42434032a8b161c4e3e8cfccd
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-08-03 11:36:44 +0800

    add bench for 'Dijkstra', 'single_source_dijkstra_path'

--- bench.py
+++ bench.py
@@ -13,18 +13,34 @@ from config import (
 sys.path.insert(0, str(eg_master_dir))
 
 from dataset_loaders import *
-from utils import eval_method
+from utils import eval_method, eg2nx, get_first_node
 
 
 def main():
-    # 遍历数据集
+    # load datasets
     for load_func_name in load_functions_name:
         cost_dict = dict()
         print(f'loading dataset with {load_func_name} ...')
-        g = eval(load_func_name)()
-        # 遍历方法
+        eg_graph = eval(load_func_name)()
+        nx_graph = eg2nx(eg_graph)
+        first_node_eg = get_first_node(eg_graph)
+        first_node_nx = get_first_node(nx_graph)
+        # bench: clustering
         for method_name in clustering_methods:
-            eval_method(cost_dict, g, load_func_name, method_name)
+            break
+            eval_method(cost_dict, eg_graph, nx_graph, load_func_name, method_name)
+
+        # bench: shortest path
+        # bench_shortest_path(cost_dict, g, load_func_name)
+        eval_method(
+            cost_dict,
+            eg_graph,
+            nx_graph,
+            load_func_name,
+            ('Dijkstra', 'single_source_dijkstra_path'),
+            call_method_args_eg=[first_node_eg],
+            call_method_args_nx=[first_node_nx],
+        )
 
 
 if __name__ == "__main__":
--- utils.py
+++ utils.py
@@ -8,6 +8,9 @@ import easygraph as eg
 import networkx as nx
 import time
 from copy import deepcopy
+from easygraph import Graph, DiGraph
+import networkx as nx
+from itertools import islice
 
 # from .types import MethodName
 
@@ -69,6 +72,7 @@ def list_allfile(path, suffix, all_files=[], all_suffix_files=[]):
 
 
 def output(data, path):
+    """dump data as json to path"""
     import json
 
     json_str = json.dumps(data, ensure_ascii=False, indent=4)
@@ -76,17 +80,26 @@ def output(data, path):
         json_file.write(json_str)
 
 
-def eg2nx(g):
+def eg2nx_graph(g: Graph) -> nx.Graph:
     G = nx.Graph()
     for v1, v2, _ in g.edges:
         G.add_edge(v1, v2)
     return G
 
 
-def eg2nx_di(g):
+def eg2nx_digraph(g: DiGraph) -> nx.DiGraph:
     G = nx.DiGraph()
     for v1, v2, _ in g.edges:
         G.add_edge(v1, v2)
+    return G
+
+
+def eg2nx(g: Union[Graph, DiGraph]) -> Union[nx.Graph, nx.DiGraph]:
+    # if load_func_name in di_load_functions_name:
+    if isinstance(g, DiGraph):
+        G = eg2nx_digraph(g)
+    else:
+        G = eg2nx_graph(g)
     return G
 
 
@@ -100,6 +113,17 @@ def json2csv(json_data, filename):
                 fw.write(metric + "," + k + "," + str(v) + "\n")
 
     fw.close()
+
+
+# class MethodCaller:
+#     def __init__(self, eg_graph, )
+# def eg_call_method(cost_dict, method_name: str, G, call_method_args_eg, call_method_kwargs_eg):
+#     start = time.time()
+#     eg_res = call_method(
+#         eg, method_name, G, call_method_args_eg, call_method_kwargs_eg
+#     )
+#     cost_dict[load_func_name][method_name]["eg"] = time.time() - start
+#     output(eg_res, load_func_name + "_" + method_name + "_eg_res.json")
 
 
 def call_method(
@@ -118,6 +142,7 @@ def call_method(
 def eval_method(
     cost_dict: dict,
     eg_graph,
+    nx_graph,
     load_func_name: str,
     method_name: 'Union[str, tuple[str, str]]',
     # method_name: Optional[str] = None,
@@ -127,80 +152,93 @@ def eval_method(
     call_method_kwargs_eg: Optional[dict] = None,
     call_method_kwargs_nx: Optional[dict] = None,
 ):
-    G = deepcopy(eg_graph)
+    G_eg = deepcopy(eg_graph)
+    G_nx = deepcopy(nx_graph)
+    load_func_name = load_func_name.removeprefix('load_')
     if isinstance(method_name, str):
         print('benchmarking method: ' + method_name)
         cost_dict[load_func_name] = dict()
         cost_dict[load_func_name][method_name] = dict()
 
-        # 加载数据
-
-        # 运行eg方法
         start = time.time()
         eg_res = call_method(
-            eg, method_name, G, call_method_args_eg, call_method_kwargs_eg
+            eg, method_name, G_eg, call_method_args_eg, call_method_kwargs_eg
         )
         cost_dict[load_func_name][method_name]["eg"] = time.time() - start
         output(eg_res, load_func_name + "_" + method_name + "_eg_res.json")
 
-        # 运行nx方法,判断转为无向图还是有向图
-        if load_func_name in di_load_functions_name:
-            G = eg2nx_di(G)
-        else:
-            G = eg2nx(G)
         start = time.time()
         nx_res = call_method(
-            nx, method_name, G, call_method_args_nx, call_method_kwargs_nx
+            nx, method_name, G_nx, call_method_args_nx, call_method_kwargs_nx
         )
         cost_dict[load_func_name][method_name]["nx"] = time.time() - start
         output(
             nx_res,
-            load_func_name.removeprefix('load_') + "_" + method_name + "_nx_res.json",
+            load_func_name + "_" + method_name + "_nx_res.json",
         )
 
         output(
             cost_dict,
-            load_func_name.removeprefix('load_') + '_' + method_name + "_cost.json",
+            load_func_name + '_' + method_name + "_cost.json",
         )
-        draw(load_func_name.removeprefix('load_') + '_' + method_name, cost_dict)
+        draw(load_func_name + '_' + method_name, cost_dict)
     elif isinstance(method_name, tuple):
         method_name_eg, method_name_nx = method_name
         print('benchmarking method: ' + method_name_eg)
         cost_dict[load_func_name] = dict()
         cost_dict[load_func_name][method_name_eg] = dict()
 
-        # 加载数据
-
-        # 运行eg方法
         start = time.time()
         eg_res = call_method(
-            eg, method_name_eg, G, call_method_args_eg, call_method_kwargs_eg
+            eg, method_name_eg, G_eg, call_method_args_eg, call_method_kwargs_eg
         )
         cost_dict[load_func_name][method_name_eg]["eg"] = time.time() - start
         output(eg_res, load_func_name + "_" + method_name_eg + "_eg_res.json")
 
-        # 运行nx方法,判断转为无向图还是有向图
-        if load_func_name in di_load_functions_name:
-            G = eg2nx_di(G)
-        else:
-            G = eg2nx(G)
         start = time.time()
         nx_res = call_method(
-            nx, method_name_nx, G, call_method_args_nx, call_method_kwargs_nx
+            nx, method_name_nx, G_nx, call_method_args_nx, call_method_kwargs_nx
         )
-        cost_dict[load_func_name][method_name_nx]["nx"] = time.time() - start
+        cost_dict[load_func_name][method_name_eg]["nx"] = time.time() - start
         output(
             nx_res,
-            load_func_name.removeprefix('load_')
-            + "_"
-            + method_name_nx
-            + "_nx_res.json",
+            load_func_name + "_" + method_name_eg + "_nx_res.json",
         )
 
         output(
             cost_dict,
-            load_func_name.removeprefix('load_') + '_' + method_name_nx + "_cost.json",
+            load_func_name + '_' + method_name_eg + "_cost.json",
         )
-        draw(load_func_name.removeprefix('load_') + '_' + method_name_nx, cost_dict)
+        draw(load_func_name + '_' + method_name_eg, cost_dict)
     else:
         raise ValueError('method_name or method_names must be specified')
+
+
+def get_first_node(g):
+    nodes = g.nodes
+    if isinstance(nodes, dict):
+        nodes_iter = nodes.keys()
+    else:
+        nodes_iter = nodes
+    return list(islice(nodes_iter, 1))[0]
+
+
+# def bench_shortest_path(cost_dict: dict, eg_graph, load_func_name: str):
+#     # the common method both libs support is dijkstra
+#     # for nx, we'll calculate the avg time
+#     G = deepcopy(eg_graph)
+#     G_nx = eg2nx(G)
+#     load_func_name = load_func_name.removeprefix('load_')
+#     print(f'benchmarking shortest path')
+#     print(f'benchmarking Dijkstra on eg')
+#     start = time.time()
+#     eg_res = call_method(eg, 'Dijkstra', G, [get_first_node(G)])
+#     cost_dict[load_func_name]['Dijkstra']['eg'] = time.time() - start
+#     output(eg_res, load_func_name + '_Dijkstra_eg_res.json')
+
+#     print(f'benchmarking single_source_dijkstra_path on nx (avg time)')
+#     start = time.time()
+#     nx_res = call_method(nx, 'dijkstra_path_length', G_nx, [get_first_node(G_nx)])
+#     cost_dict[load_func_name]['Dijkstra']['nx'] = time.time() - start
+#     output(nx_res, load_func_name + '_Dijkstra_nx_res.json')
+#     draw(load_func_name + '_Dijkstra', cost_dict)
