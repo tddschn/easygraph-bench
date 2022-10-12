b73099c4404d2d9cba302524623d2bf0bbbcd9ee
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-10-12 11:53:56 +0800

    gen_bench_script.py: supports generation of entrypoint.sh

--- gen_bench_script.py
+++ gen_bench_script.py
@@ -6,10 +6,13 @@ Purpose: generate bench scripts from jinja template
 """
 
 import argparse
+from io import StringIO
 from jinja2 import FileSystemLoader, Environment, Template
 from pathlib import Path
 from config import dataset_names, bench_scripts_set
 from stat import S_IEXEC
+
+ENTRYPOINT_SH_PATH = Path(__file__).parent / 'entrypoint.sh'
 
 
 def gen_bench_script(dataset_name: str, template: Template) -> str:
@@ -20,6 +23,13 @@ def gen_bench_script(dataset_name: str, template: Template) -> str:
 def gen_bench_script_entrypoint(script_set: str, template: Template) -> str:
     script_set_list = f'{script_set}'
     return template.render(script_set_list=script_set_list)
+
+
+def gen_bench_script_entrypoint_bash(
+    datasets: list[str], bash_arg: str, template: Template
+) -> str:
+    script_body = '\n'.join(f'./bench_{d}.py {bash_arg}' for d in datasets)
+    return template.render(script_body=script_body)
 
 
 def get_args():
@@ -45,7 +55,20 @@ def get_args():
         help='generate entry point scripts instead of individual ones',
         action='store_true',
     )
+
+    parser.add_argument(
+        '-E',
+        '--entrypoint-bash',
+        help='generate entry point scripts in bash',
+        action='store_true',
+    )
+
+    parser.add_argument(
+        '--bash-arg', help='command line args for entrypoint.sh', type=str, default='$@'
+    )
+
     parser.add_argument('-o', '--output', type=Path, help='output file')
+
     return parser.parse_args()
 
 
@@ -56,6 +79,7 @@ def main():
     # Load the Jinja2 template.
     template = e.get_template('bench_template.jinja.py')
     template_entrypoint = e.get_template('entrypoint.jinja.py')
+    template_entrypoint_bash = e.get_template('entrypoint.jinja.sh')
     if args.entrypoint:
         for script_set in bench_scripts_set:
             script_content = gen_bench_script_entrypoint(
@@ -68,6 +92,17 @@ def main():
             print(
                 f'Benchmark entrypoint script for {script_set_name} generated at {output_path}'
             )
+        return
+
+    if args.entrypoint_bash:
+        script_content = gen_bench_script_entrypoint_bash(
+            datasets=args.dataset,
+            bash_arg=args.bash_arg,
+            template=template_entrypoint_bash,
+        )
+        ENTRYPOINT_SH_PATH.write_text(script_content)
+        ENTRYPOINT_SH_PATH.chmod(ENTRYPOINT_SH_PATH.stat().st_mode | S_IEXEC)
+        print(f'Benchmark entrypoint shell script generated at {ENTRYPOINT_SH_PATH}')
         return
 
     if args.output is not None and len(args.dataset) == 1:
