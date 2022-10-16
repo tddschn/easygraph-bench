f649be738c09e44a94b29d0db0b7d8ecfa1122b7
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-10-16 23:39:30 +0800

    add gen_bench_script.py

--- gen_bench_script.py
+++ gen_bench_script.py
@@ -7,9 +7,17 @@ Purpose: generate bench scripts from jinja template
 
 import argparse
 from io import StringIO
+import json
 from jinja2 import FileSystemLoader, Environment, Template
 from pathlib import Path
-from config import dataset_names, bench_scripts_set
+from config import (
+    graph_info_json_path,
+    dataset_names,
+    bench_scripts_set,
+    read_profile_preparation_code,
+    graph_benchmark_code_json_path,
+    edgelist_filenames,
+)
 from stat import S_IEXEC
 
 ENTRYPOINT_SH_PATH = Path(__file__).parent / 'entrypoint.sh'
@@ -30,6 +38,17 @@ def gen_bench_script_entrypoint_bash(
 ) -> str:
     script_body = '\n'.join(f'./bench_{d}.py {bash_arg}' for d in datasets)
     return template.render(script_body=script_body)
+
+
+def gen_profile_script(
+    tool: str, method_to_code_mapping: dict[str, str], template: Template
+) -> str:
+    profile_preparation_code = read_profile_preparation_code()[tool]
+    return template.render(
+        tool=tool,
+        profile_preparation_code=profile_preparation_code,
+        graph_benchmark_code=method_to_code_mapping,
+    )
 
 
 def get_args():
@@ -64,7 +83,21 @@ def get_args():
     )
 
     parser.add_argument(
-        '--bash-arg', help='command line args for entrypoint.sh', type=str, default='$@'
+        '--bash-arg',
+        help='command line args for entrypoint.sh',
+        type=str,
+        default='"$@"',
+    )
+
+    parser.add_argument(
+        '-P', '--profile', help='generate profile scripts', action='store_true'
+    )
+
+    parser.add_argument(
+        '-Z',
+        '--profile-entrypoint',
+        help='generate profile entrypoint scripts',
+        action='store_true',
     )
 
     parser.add_argument('-o', '--output', type=Path, help='output file')
@@ -80,6 +113,7 @@ def main():
     template = e.get_template('bench_template.jinja.py')
     template_entrypoint = e.get_template('entrypoint.jinja.py')
     template_entrypoint_bash = e.get_template('entrypoint.jinja.sh')
+    template_profile_script = e.get_template('profile_template.jinja.py')
     if args.entrypoint:
         for script_set in bench_scripts_set:
             script_content = gen_bench_script_entrypoint(
@@ -103,6 +137,47 @@ def main():
         ENTRYPOINT_SH_PATH.write_text(script_content)
         ENTRYPOINT_SH_PATH.chmod(ENTRYPOINT_SH_PATH.stat().st_mode | S_IEXEC)
         print(f'Benchmark entrypoint shell script generated at {ENTRYPOINT_SH_PATH}')
+        return
+
+    loading_methods = {'loading': '', 'loading_undirected': '_undirected'}
+    if args.profile:
+        gbc = json.loads(graph_benchmark_code_json_path.read_text())
+        for tool, method_to_code_mapping in gbc.items():
+            for loading_method, script_name_suffix in loading_methods.items():
+                m = method_to_code_mapping.copy()
+                if loading_method not in m:
+                    continue
+                methods_to_pop = []
+                if loading_method == 'loading':
+                    methods_to_pop.append('loading_undirected')
+                if loading_method == 'loading_undirected':
+                    methods_to_pop.append('loading')
+                    methods_to_pop += ['page rank', 'strongly connected components']
+                [m.pop(method_to_pop, None) for method_to_pop in methods_to_pop]
+                script_content = gen_profile_script(tool, m, template_profile_script)
+                output_path = Path(f'profile_{tool}{script_name_suffix}.py')
+                output_path.write_text(script_content)
+                output_path.chmod(output_path.stat().st_mode | S_IEXEC)
+                print(f'Profile script for {tool} generated at {output_path}')
+        return
+
+    if args.profile_entrypoint:
+        output_path = Path('profile_entrypoint.sh')
+        script_lines = ['#!/usr/bin/env bash', '']
+        gi = json.loads(graph_info_json_path.read_text())
+        gbc = json.loads(graph_benchmark_code_json_path.read_text())
+        for edgelist_path in edgelist_filenames:
+            p = Path(edgelist_path)
+            dataset_name = p.stem
+            # print(dataset_name)
+            for tool in gbc:
+                is_directed = gi[dataset_name]['is_directed']
+                script_name_suffix = '_undirected' if not is_directed else ''
+                script_filename = f'profile_{tool}{script_name_suffix}.py'
+                script_lines.append(f'./{script_filename} {edgelist_path} "$@"')
+        output_path.write_text('\n'.join(script_lines))
+        output_path.chmod(output_path.stat().st_mode | S_IEXEC)
+        print(f'Profile entrypoint script generated at {output_path}')
         return
 
     if args.output is not None and len(args.dataset) == 1:
