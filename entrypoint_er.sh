0860e4aab5251465c67b1966032f8b9ab5dfc2e0
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-10-30 14:29:46 +0000

    gen scripts

--- entrypoint_er.sh
+++ entrypoint_er.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
-./bench_er_200_directed.py "$@"
-./bench_er_200.py "$@"
-./bench_er_500_directed.py "$@"
 ./bench_er_500.py "$@"
 ./bench_er_1000.py "$@"
-./bench_er_1000_directed.py "$@"
-./bench_er_2000.py "$@"
-./bench_er_2000_directed.py "$@"
-./bench_er_5000_directed.py "$@"
 ./bench_er_5000.py "$@"
+./bench_er_10000.py "$@"
