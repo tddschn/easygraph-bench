aa3a87ef5b55692d1f8b2dd24e364bab5097e52b
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-10-30 19:06:20 +0800

    2022-10-30T19-06-20Z

--- gen_er.py
+++ gen_er.py
@@ -6,6 +6,7 @@ Purpose: Generate ER random graph datasets
 """
 
 import argparse
+from pathlib import Path
 
 
 # n_nodes = [200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]
@@ -47,9 +48,10 @@ def main():
 
     num_nodes = args.nodes if args.nodes != n_nodes else n_nodes
 
+    Path('dataset/random-erdos-renyi').mkdir(parents=True, exist_ok=True)
     for num in num_nodes:
         for directed in (True, False):
-            filepath = f'dataset/random-erdos-renyi/er{"_directed" if directed else ""}.edgelist'
+            filepath = f'dataset/random-erdos-renyi/{num}{"_directed" if directed else ""}.edgelist'
             er(num, args.p, directed, filepath)
 
 
