0fda78602f21a5cba3af998cd33a671eba2f2a02
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2022-12-08 19:29:27 +0800

    update gen_er.py
    from Min Gao:
    增加multiprocessing和hybrid programming针对random graph的benckmark， 其中node size分别取0.5k, 5k, 50k, 500k, 1000k的不同algorithm的runtime效果（之前生成ER图的参数p的设定问题可以采用另一个function：erdos_renyi_M， 可以直接设定edge size的大小）

--- gen_er.py
+++ gen_er.py
@@ -7,18 +7,28 @@ Purpose: Generate ER random graph datasets
 
 import argparse
 from pathlib import Path
+import sys
 
 
 # n_nodes = [200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]
 # n_nodes = [200, 500, 1000, 2000, 5000, 10000]
-n_nodes = [500, 1000, 5000, 10000]
+# n_nodes = [500, 1000, 5000, 10000]
+
+# 0.5k, 5k, 50k, 500k, 1000k
+n_nodes = [500, 5000, 50000, 500000, 1000000]
 
 
-def get_p(num_nodes: int, edge_to_node_ratio: float | int = 5) -> float:
+# def get_p(num_nodes: int, edge_to_node_ratio: float | int = 5) -> float:
+#     total_possible_edges = num_nodes * (num_nodes - 1) / 2
+#     # ratio = tot * p / nodes
+#     # p = ratio * nodes / tot
+#     return edge_to_node_ratio * num_nodes / total_possible_edges
+
+
+def get_p(num_nodes: int, edges_number: int) -> float:
     total_possible_edges = num_nodes * (num_nodes - 1) / 2
-    # ratio = tot * p / nodes
-    # p = ratio * nodes / tot
-    return edge_to_node_ratio * num_nodes / total_possible_edges
+    # p = edges / total_possible_edges
+    return edges_number / total_possible_edges
 
 
 def get_args():
@@ -56,6 +66,35 @@ def get_args():
     return parser.parse_args()
 
 
+# def main():
+#     """Make a jazz noise here"""
+
+#     args = get_args()
+
+#     import easygraph as eg
+
+#     er = eg.erdos_renyi_P
+
+#     num_nodes = args.nodes if args.nodes != n_nodes else n_nodes
+
+#     Path('dataset/random-erdos-renyi').mkdir(parents=True, exist_ok=True)
+#     directed_option_list = [False] + ([True] if args.directed else [])
+#     total_graph_count = len(num_nodes) * len(directed_option_list)
+#     idx = 1
+#     for num in num_nodes:
+#         for directed in directed_option_list:
+#             filepath = f'dataset/random-erdos-renyi/{num}{"_directed" if directed else ""}.edgelist'
+#             print(
+#                 f'Generating {filepath} ({"directed" if directed else "undirected"}) {idx} / {total_graph_count}...'
+#             )
+#             if args.p is not None:
+#                 p = args.p
+#             else:
+#                 p = get_p(num)
+#             er(num, p, directed, filepath)
+#             idx += 1
+
+
 def main():
     """Make a jazz noise here"""
 
@@ -79,8 +118,13 @@ def main():
             )
             if args.p is not None:
                 p = args.p
+            elif args.edges_number is not None:
+                # Use the specified number of edges to calculate p
+                p = get_p(num, args.edges_number)
             else:
-                p = get_p(num)
+                # Use the default edge-to-node ratio to calculate p
+                # p = get_p(num)
+                sys.exit(f'Error: Either p or edges_number must be specified')
             er(num, p, directed, filepath)
             idx += 1
 
