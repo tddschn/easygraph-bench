ad1826a46330f2526dc8f6caddb88da5c89c1365
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-03-24 16:34:11 +0800

    2023-03-24T16-34-11Z

---
+++ get_lcc_edgelist.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""
+Author : Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
+Date   : 2023-03-24
+Purpose: Why not?
+"""
+
+import argparse
+from pathlib import Path
+import networkx as nx
+from config import edgelist_filenames
+
+def load_graph(filename: str) -> nx.Graph:
+    return nx.read_edgelist(filename, delimiter="\t", nodetype=int, create_using=nx.Graph())
+
+def get_lcc(g: nx.Graph) -> nx.Graph:
+    return nx.subgraph(g, max(nx.connected_components(g), key=len))
+
+def write_graph(g: nx.Graph, path: str):
+    nx.write_edgelist(g, path, delimiter="\t")
+
+
+
+def get_args():
+    """Get command-line arguments"""
+
+    parser = argparse.ArgumentParser(
+        description='Why not?',
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+
+
+    return parser.parse_args()
+
+
+def main():
+    """Make a jazz noise here"""
+
+    args = get_args()
+    for filename in edgelist_filenames:
+        g = load_graph(filename)
+        lcc = get_lcc(g)
+        write_graph(lcc, str(Path(filename).with_name(f"{Path(filename).stem}_lcc.edgelist")))
+        print(f'Wrote {filename} to {Path(filename).with_name(f"{Path(filename).stem}_lcc.edgelist")}')
+
+
+if __name__ == '__main__':
+    main()
