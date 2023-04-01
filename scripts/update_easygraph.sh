7518d19581cc205c7001bfc77a4ee4898fac8942
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-04-01 14:31:01 +0800

    2023-04-01T14-31-01Z

--- scripts/update_easygraph.sh
+++ scripts/update_easygraph.sh
@@ -3,6 +3,9 @@
 # set default path to Easy-Graph repo dir
 repo_path="${1:-$HOME/Easy-Graph}"
 
+# remember current directory
+orig_dir="$(pwd)"
+
 # print help message if user passes --help flag
 if [[ $1 == "--help" ]]; then
   echo "Usage: ./update_easygraph.sh [PATH]"
@@ -13,12 +16,17 @@ fi
 
 # cd to repo dir and update using git pull
 cd "$repo_path" || exit
+before=$(git rev-parse HEAD)
 git pull > /dev/null
+after=$(git rev-parse HEAD)
 
 # if updates are pulled, run setup.py install and inform user
-if [[ $(git diff --stat) != '' ]]; then
+if [[ "$before" != "$after" ]]; then
   echo "Updates pulled from Easy-Graph repo."
   python3 setup.py install
 else
   echo "No updates available for Easy-Graph repo."
 fi
+
+# change back to original directory
+cd "$orig_dir" || exit
