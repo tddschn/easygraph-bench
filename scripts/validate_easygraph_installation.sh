aaca31d39ab68f402a49424d70c31d7a91192475
Teddy Xinyuan Chen <45612704+tddschn@users.noreply.github.com>
2023-04-03 23:12:29 +0800

    2023-04-03T23-12-29Z

--- scripts/validate_easygraph_installation.sh
+++ scripts/validate_easygraph_installation.sh
@@ -5,3 +5,8 @@ source venv/bin/activate
 pip install python-easygraph tensorflow
 
 python3 -c 'import easygraph as eg; eg.complete_graph(5).cpp()'
+
+pip install pebble hr-tddschn
+
+cd ~/easygraph-bench || exit
+./profile_entrypoint_cpp-sanity-check.sh
